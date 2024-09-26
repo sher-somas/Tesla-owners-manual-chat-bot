c3ba621372932f4f13c3331f95e29de311778ffb
sher-somas <sher.somas@gmail.com>
2024-09-26 18:15:02 +0200

    contains twilio whatsapp message send logic

---
+++ whatsapp.py
@@ -0,0 +1,9 @@
+from twilio.rest import Client
+from dotenv import load_dotenv
+
+twilio_client = Client()
+load_dotenv()
+
+def send_message(llm_output):
+    twilio_client.messages.create(from_="whatsapp:+14155238886",
+                                        body=llm_output, to="whatsapp:+4915219432029")
