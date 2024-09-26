1682c4b6c36f361f2c1b0fbb9641781297f8edcb
sher-somas <sher.somas@gmail.com>
2024-09-26 21:41:23 +0200

    code formatting and linting with comments

--- app.py
+++ app.py
@@ -1,27 +1,56 @@
-from langchain_openai import OpenAIEmbeddings
 from dotenv import load_dotenv
-from pinecone import Pinecone as PineconeClient
 from langchain_community.vectorstores import Pinecone
-from langchain_core.runnables import RunnableParallel, RunnablePassthrough
-from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+from langchain_openai import OpenAIEmbeddings
 from langchain_together import ChatTogether
-
-
+from pinecone import Pinecone as PineconeClient
 
 load_dotenv()
 
 llm = ChatTogether(temperature=0.0, model="meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo")
 
-index_name = 'tesla-manuals'
+index_name = "tesla-manuals"
 
 pinecone_client = PineconeClient()
 embeddings = OpenAIEmbeddings()
 vector_store = Pinecone.from_existing_index(index_name=index_name, embedding=embeddings)
 retriever = vector_store.as_retriever()
 
-def invoke_chain(question):
-    # RAG prompt
+
+def invoke_chain(question: str) -> str:
+    """
+    Invokes a chain of operations to retrieve relevant information from the Pinecone vector store and generate an answer using the Meta-Llama 3.1 model.
+
+    Args:
+        question (str): The user's input question.
+
+    Returns:
+        str: The generated answer based on the retrieved context and question.
+
+    Create a ChatPromptTemplate instance from the template
+    prompt = ChatPromptTemplate.from_template(template)
+
+    # Define a chain of operations using the RunnableParallel and RunnablePassthrough classes
+    chain = (
+        # Retrieve relevant context from the Pinecone vector store
+        RunnableParallel({"context": retriever, "question": RunnablePassthrough()})
+
+        # Apply the RAG prompt to the retrieved context and question
+        | prompt
+
+        # Use the Meta-Llama 3.1 model to generate an answer based on the applied prompt
+        | ChatTogether(temperature=0, model="meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo")
+
+        # Parse the generated output as a string
+        | StrOutputParser()
+    )
+
+    # Invoke the chain of operations and return the generated answer
+    return chain.invoke(question)
+    """
+
     template = """You are a Tesla car manual bot and have access to all the owner's manual of all the tesla car models ever produced.
     Answer the question with confidence and a friendly manner. If you don't know the answer, say I don't know.
 
@@ -31,7 +60,9 @@ def invoke_chain(question):
     prompt = ChatPromptTemplate.from_template(template)
 
     # RAG
-    model = ChatTogether(temperature=0, model="meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo")
+    model = ChatTogether(
+        temperature=0, model="meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo"
+    )
 
     chain = (
         RunnableParallel({"context": retriever, "question": RunnablePassthrough()})
@@ -39,6 +70,5 @@ def invoke_chain(question):
         | model
         | StrOutputParser()
     )
-
 
     return chain.invoke(question)
--- main.py
+++ main.py
@@ -1,38 +1,87 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import Body, FastAPI, HTTPException
 from fastapi import status as response_status
 from pydantic import BaseModel
+
 from app import invoke_chain
 from whatsapp import send_message
-from fastapi import Body
 
+app = FastAPI(
+    title="Tesla owner's manual",
+    version="0.0.1",
+    contact={
+        "name": "Shreyas Somashekar",
+        "email": "sher.somas@gmail.com",
+    },
+)
 
-app = FastAPI()
 
 class Question(BaseModel):
-    input_str : str
+    input_str: str
 
 
 @app.get("/")
 def ping_check():
+    """
+    Endpoint to handle GET requests to the root URL ("/"), returning a simple "Hello World" response.
+
+    Returns:
+        dict: A dictionary containing the greeting message.
+    """
     return {"Hello": "World"}
 
+
 @app.post("/question")
-def translate(request: Question, status=response_status.HTTP_200_OK):
-    
+def web_request(request: Question, status=response_status.HTTP_200_OK):
+    """
+    Web-based question answering endpoint to handle POST requests containing user input.
+
+    Args:
+        request (Question): A Question object containing the user's input string.
+
+    Returns:
+        dict: A dictionary containing the translated output from the LLM.
+
+    Raises:
+        HTTPException: If an error occurs during processing, returns a 500 Internal Server Error response with the exception details.
+
+    Notes:
+        This endpoint is designed for handling web-based requests. It takes user input as a string, translates it using the LLM, and returns the result in JSON format.
+    """
     try:
         llm_output = invoke_chain(request.input_str)
         return {"response": llm_output}
     except Exception as e:
-        raise HTTPException(status_code=response_status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+        raise HTTPException(
+            status_code=response_status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
+        )
+
 
 @app.post("/question/whatsapp")
-def translate(request: str = Body(..., media_type="application/json"), status=response_status.HTTP_200_OK):
-    
+def whatsapp_request(
+    request: str = Body(..., media_type="application/json"),
+    status=response_status.HTTP_200_OK,
+):
+    """
+    WhatsApp messaging endpoint to handle POST requests containing user input.
+
+    Args:
+        request (str): The user's input string as a JSON payload.
+
+    Returns:
+        dict: A dictionary containing the translated output from the LLM and sent to WhatsApp.
+
+    Raises:
+        HTTPException: If an error occurs during processing, returns a 500 Internal Server Error response with the exception details.
+
+    Notes:
+        This endpoint is designed for sending automated messages via WhatsApp. It takes user input as a string, translates it using the LLM, and sends the result to WhatsApp using the `send_message` function.
+    """
     message = request.lower()
     try:
         llm_output = invoke_chain(message)
         send_message(llm_output)
         return {"response": llm_output}
     except Exception as e:
-        raise HTTPException(status_code=response_status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
-
+        raise HTTPException(
+            status_code=response_status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
+        )
